93695376611cddc4c6a2fc66fcf2900c5c65b889
ProgrammingStairs <programmingstairseducation@gmail.com>
2025-02-22 15:41:44 +0530

    Add files via upload

---
+++ expressjs/3.1 Template Engine Webpage/app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+var app = express();
+
+app.use(express.static('public'));
+app.set("views","views");
+app.set("view engine","ejs");
+
+app.get("/",(request,response)=>{
+    response.render("index.ejs");    
+});
+
+app.listen(3000,()=>{
+    console.log("Connection established successfully");
+});
